3d100da4ae7ef5600101cecb50236ca4219839e7
Douglas <douglas.cavalcanti@ufu.br>
2017-04-28 10:20:54 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = angular.module('app', []);
+
+app.controller('mainController', function($scope){
+    
+  $scope.form = [
+
+    {
+      id: 0,//open question
+      type: 0,
+      content: {
+        text: "What is your opinion about the operation of the software?"
+      }
+    },
+
+    {
+      id: 1,
+      type: 1,//multiple choice question
+      content: {
+        text: "Select the language you work with",
+        items: ["Java", "JavaScript", "Python"]
+      }
+    },
+
+    {
+      id: 2,
+      type: 2,//evaluation of items
+      content: {
+        text: "Evaluate the items below:",
+        items: ["Item A", "Item B", "Item C"]
+      }
+    }
+
+  ];
+
+  $scope.answers = [];
+
+  $scope.sendForm = function () {
+    console.log($scope.answers);
+  }
+
+});
+
+
